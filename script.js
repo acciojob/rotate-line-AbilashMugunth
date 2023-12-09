99240e75c048a31ce62319c3a7aa5b64c8160fd9
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-09 22:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.querySelector("#line");
+
+let currentRotation = 0;
+
+function rotate() {
+  line.style.transform = `rotate(${currentRotation}deg)`;
+  currentRotation += 2;
+}
+
+setInterval(rotate, 20);
